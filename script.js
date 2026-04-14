6a9a1bc58891346915ad6e818a68287ea2e301d7
serigneka <serigneka445@gmail.com>
2026-04-14 13:17:38 +0000

    jai ajouter des modif

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 
+
+
+
 let citations = [
-    // citation 1
-    "« Si tu veux aller vite, marche seul. Si tu veux aller loin, marchons ensemble. »",
-    // citation 2
-     "« L’enfant qui n’est pas embrassé par le village le brûlera pour sentir sa chaleur. »",
-    // citation 3
- "« Quand les racines d’un arbre commencent à pourrir, il répand la mort sur les branches. »",
-    // citation 4
-  "« Un seul doigt ne peut pas ramasser un caillou. »",
-    // citation 5
-  "« Le lion ne se retourne pas quand un petit chien aboie. »",
-    // citation 6
+    " « Si tu veux aller vite, marche seul. Si tu veux aller loin, marchons ensemble. » " ,
+     " « L’enfant qui n’est pas embrassé par le village le brûlera pour sentir sa chaleur. »",
+ " « Quand les racines d’un arbre commencent à pourrir, il répand la mort sur les branches. »",
+  " « Un seul doigt ne peut pas ramasser un caillou. »",
+  " « Le lion ne se retourne pas quand un petit chien aboie. »",
   "« Même le meilleur danseur doit quitter la scène. »",
-    // citation 7
-  "« La sagesse est comme un baobab : une seule personne ne peut pas l’embrasser. »",
-    // citation 8
-  "« La sagesse est comme un baobab : une seule personne ne peut pas l’embrasser. »",
-    // citation 9
-  "« L’eau chaude n’oublie pas qu’elle a été froide. »",
-    // citation 10
- "« Celui qui pose des questions ne se perd jamais. »", 
+  "  « La sagesse est comme un baobab : une seule personne ne peut pas l’embrasser. »",
+  "   « La sagesse est comme un baobab : une seule personne ne peut pas l’embrasser. »",
+  "  « L’eau chaude n’oublie pas qu’elle a été froide. »",
+ "  « Celui qui pose des questions ne se perd jamais. »", 
 ]
-
-
-const bouton = document.getElementById("new-code");
+const bouton = document.getElementById("new-citation");
 const zoneCitation = document.getElementById("citations");
 
 bouton.addEventListener("click", function() {
 
 const index = Math.floor(Math.random() * citations.length);
 zoneCitation.innerText = citations[index];
-});
+zoneCitation.style.backgroundColor = "rgb(66, 60, 147)";
 
+  let randomColor = '#' + Math.floor(Math.random() * 16787215).toString(16);
+document.body.style.backgroundColor = randomColor;
+});
